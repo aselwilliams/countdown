f0561213c5f93734fcb764bea9ca5757f597e707
aselwilliams <aselwilliams22@gmail.com>
2022-05-14 10:08:04 -0400

    timer is working properly

--- src/App.js
+++ src/App.js
@@ -2,16 +2,36 @@ import {useState, useEffect} from 'react'
 import './App.css';
 
 function App() {
-  const [time, setTime]=useState(0);
-  const [timerOn, setTimerOn] =useState(false)
+  const [time, setTime]=useState(1405000);
+  const [timerOn, setTimerOn] =useState(true);
+
+  useEffect(()=>{
+    let interval=null;
+    if(timerOn) {
+      interval = setTimeout(()=> {
+        setTime(prevState=>prevState-1000)
+    },1000);
+    } else {
+      clearTimeout(interval)
+    }
+    if(time==0){
+      clearTimeout(interval)
+  } 
+  return ()=>clearTimeout(interval)
+  },[timerOn][time])
+
   return (
-    <div className="App">
-     <main>
-        <div id="display-timer">00:20:33</div>
-        <button onClick={()=>setTimerOn(false)} id="stop-timer">Stop Timer</button>
-        <button onClick={()=>setTimerOn(true)} id="stop-timer">Resume Timer</button>
-        <button onClick={()=>setTimerOn(true)} id="stop-timer">Start Timer</button>
-        <button onClick={()=>setTime(0)} id="stop-timer">Reset Timer</button>
+    <div>
+     <main className='card-box'>
+        <h1>Countdown</h1>
+        <div id="display-timer">
+            <span>{('0' + Math.floor((time / 3600000) % 60)).slice(-2)} : </span>
+            <span>{('0' + Math.floor((time / 60000) % 60)).slice(-2)} : </span>
+            <span>{('0' + ((time / 1000) % 60)).slice(-2)}</span>
+        </div>
+        <button onClick={()=>setTimerOn(!timerOn)} className={timerOn ? 'stop' : 'resume'}>
+          {timerOn ? 'Stop Timer' : 'Resume Timer'}
+        </button>
     </main>
     </div>
   );
